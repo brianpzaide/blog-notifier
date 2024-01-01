23d64487e92480b9418dae568588be583a5c522c
brianpzaide <brianpzaide@gmail.com>
2024-01-01 14:24:40 +0000

    feat: creating sample solution for stage3

---
+++ samplesolution/stage3/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+// finds all the links in a blog post
+func findAllLinks(site string) ([]string, error) {
+	res, err := http.Get(site)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return nil, err
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	links := make([]string, 0)
+
+	// Find the review items
+	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the title
+		link, exists := s.Attr("href")
+		if exists {
+			links = append(links, link)
+		}
+	})
+	return links, nil
+}
+
+// recursive crawl function
+func _crawl(link string, links *[]string) error {
+	_links, err := findAllLinks(link)
+	if err == nil {
+		for _, _link := range _links {
+			*links = append(*links, _link)
+			err := _crawl(_link, links)
+			if err != nil {
+				return fmt.Errorf("%s: error in recursive crawl", link)
+			}
+		}
+		return nil
+	} else {
+		return fmt.Errorf("%s: error in finAllLinks", link)
+	}
+}
+
+// implements the crawl functionality
+func crawl(blogSite string) ([]string, error) {
+	// crawl the sites
+
+	links := make([]string, 0)
+	err := _crawl(blogSite, &links)
+	if err != nil {
+		return nil, err
+	} else {
+		uniqueLinksMap := make(map[string]bool)
+		uniqueLinks := make([]string, 0)
+		for _, _link := range links {
+			if _, ok := uniqueLinksMap[_link]; !ok {
+				uniqueLinks = append(uniqueLinks, _link)
+				uniqueLinksMap[_link] = true
+			}
+		}
+		return uniqueLinks, nil
+	}
+
+}
+
+func main() {
+	// Parse command-line arguments
+	crawlFlag := flag.String("crawl", "", "Crawl web links")
+
+	flag.Parse()
+
+	if *crawlFlag != "" {
+		fmt.Println("crawl")
+		_links, err := crawl(*crawlFlag)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, _link := range _links {
+			fmt.Println(_link)
+		}
+	}
+}
